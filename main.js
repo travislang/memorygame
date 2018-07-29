5c4f43377f02a4f58b8f36d2131d18c0d03b84cf
t.lang <38336774+tlang50@users.noreply.github.com>
2018-07-28 23:15:13 -0500

    add further game logic

--- main.js
+++ main.js
@@ -11,35 +11,69 @@ window.onload = function() {
 	let highScore = 0;
 	let pattern = [];
 	let colors = [red, blue, yellow, green];
+	let j = 0;
 
 	startButton.addEventListener("click", function() {
-		console.log('hello')
 		pattern = [];
 		currScore = 0;
-		current = 3;
 		currScoreDisp.innerHTML = currScore;
 		start();
 	})
 
 	function start() {
-		console.log("game about to start")
+		console.log("game about to start");
 		setTimeout(function() {
+		pattern.push(random());
 		nextTurn();
 		}, 1000);
 	}
 
 	function nextTurn() {
-		if (pattern.length > current) return;
-		pattern.push(random());
-		colors[x].classList.add("lighten");
+		if (j > pattern.length - 1) {
+			j = 0;
+			return;
+		}
+		colors[pattern[j]].classList.add("lighten");
 		setTimeout(function() {
-			colors[x].classList.remove("lighten");
-			pattern.push(x);
+			colors[pattern[j]].classList.remove("lighten");
 			console.log(pattern);
+			j++;
 			setTimeout(nextTurn, 500);
 		}, 500);
 	}
+	function userTurn() {
+		if (this === colors[pattern[j]]) {
+			console.log("correct");
+			console.log(j)
+			j++;
+			if (j === pattern.length) {
+				currScore++;
+				console.log(currScore);
+				j = 0;
+				pattern.push(random());
+				setTimeout(nextTurn, 1000);
+			}
+		}
+		else {
+			console.log('wrong');
+			j = 0;
+			currScore = 0;
+			console.log(currScore);
+		}
 
+	}
+	red.addEventListener("click", function() {
+		userTurn.call(this);
+	})
+	blue.addEventListener("click", function() {
+		userTurn.call(this);
+	})
+	yellow.addEventListener("click", function() {
+		userTurn.call(this);
+	})
+	green.addEventListener("click", function() {
+		userTurn.call(this);
+	})
 	function random() {
 		return Math.floor(Math.random() * 4)
 	}
