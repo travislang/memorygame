c211eaec4dcd5eaf8310e257275164df20b85103
t.lang <38336774+tlang50@users.noreply.github.com>
2018-07-28 21:49:18 -0500

    add basic funtion
    add selectors
    add eventlisteners
    
    and basic random patter

--- main.js
+++ main.js
@@ -1,1 +1,46 @@
+window.onload = function() {
+	const startButton = document.querySelector("button"),
+			red = document.querySelector(".red"),
+			blue = document.querySelector(".blue"),
+			yellow = document.querySelector(".yellow"),
+			green = document.querySelector(".green"),
+			currScoreDisp = document.querySelector(".current-score"),
+			highScoreDisp = document.querySelector(".highest-score");
 
+	let currScore = 0;
+	let highScore = 0;
+	let pattern = [];
+	let colors = [red, blue, yellow, green];
+
+	startButton.addEventListener("click", function() {
+		console.log('hello')
+		pattern = [];
+		currScore = 0;
+		current = 3;
+		currScoreDisp.innerHTML = currScore;
+		start();
+	})
+
+	function start() {
+		console.log("game about to start")
+		setTimeout(function() {
+		nextTurn();
+		}, 1000);
+	}
+
+	function nextTurn() {
+		if (pattern.length > current) return;
+		pattern.push(random());
+		colors[x].classList.add("lighten");
+		setTimeout(function() {
+			colors[x].classList.remove("lighten");
+			pattern.push(x);
+			console.log(pattern);
+			setTimeout(nextTurn, 500);
+		}, 500);
+	}
+
+	function random() {
+		return Math.floor(Math.random() * 4)
+	}
+};
